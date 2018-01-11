c1bc9ca8e66183b7e26cd42c19e59ad03653a1ea
MaximeBouton <maxim.bouton@gmail.com>
2018-01-11 14:12:28 -0800

    renamed intersection

---
+++ single_intersection/pomdp_types.jl
@@ -0,0 +1,187 @@
+
+"""
+Representation of a state for the OIcludedIntersection POMDP,
+depends on ADM VehicleState type
+"""
+mutable struct OIState
+    crash ::Bool
+    ego::VehicleState
+    car::VehicleState
+end
+
+# copy b to a
+function Base.copy!(a::OIState, b::OIState)
+    a.crash = b.crash
+    a.ego = b.ego
+    a.car = b.car
+end
+
+function Base.hash(s::OIState, h::UInt64 = zero(UInt64))
+    return hash(s.crash, hash(s.ego, hash(s.car, h)))
+end
+
+function Base.:(==)(a::OIState, b::OIState)
+    return a.crash == b.crash && a.ego == b.ego && a.car == b.car
+end
+
+#### Observaton type
+
+const OIObs = OIState
+
+#### Action type
+
+mutable struct OIAction
+    acc::Float64
+end
+
+function Base.copy!(a::OIAction, b::OIAction)
+    a.acc = b.acc
+end
+
+function Base.hash(a::OIAction, h::UInt64 = zero(UInt64))
+    return hash(a.acc, h)
+end
+
+function Base.:(==)(a::OIAction, b::OIAction)
+    return a.acc == b.acc
+end
+
+#### POMDP type
+
+mutable struct OIPOMDP <: POMDP{OIState, OIAction, OIObs}
+    env::IntersectionEnv
+    ego_type::VehicleDef
+    car_type::VehicleDef
+    max_acc::Float64
+    pos_res::Float64
+    vel_res::Float64
+    s_start::Float64
+    s_goal::Float64
+    off_grid::VecSE2
+    ΔT::Float64 # decision frequency
+    p_birth::Float64
+    a_noise::Float64
+    pos_obs_noise::Float64
+    vel_obs_noise::Float64
+    collision_cost::Float64
+    action_cost::Float64
+    goal_reward::Float64
+    γ::Float64 # discount factor
+end
+
+function OIPOMDP(; env::IntersectionEnv = IntersectionEnv(),
+                   ego_type::VehicleDef = VehicleDef(),
+                   car_type::VehicleDef = VehicleDef(AgentClass.CAR, 6.0, 3.0),
+                   max_acc::Float64 = 2.0,
+                   pos_res::Float64 = 2.,
+                   vel_res::Float64 = 2.,
+                   s_start::Float64 = env.params.stop_line - ego_type.length/2,
+                   s_goal::Float64 = 30.,
+                   off_grid::VecSE2 = VecSE2(14., -5, pi/2),
+                   ΔT::Float64  = 0.5,
+                   p_birth::Float64 = 0.1,
+                   a_noise::Float64 = 1.0,
+                   pos_obs_noise::Float64 = 1.0,
+                   vel_obs_noise::Float64 = 1.0,
+                   collision_cost::Float64 = -1.,
+                   action_cost::Float64 = 0.0,
+                   goal_reward::Float64 = 1.,
+                   γ::Float64  = 0.95)
+    return OIPOMDP(env,
+                   ego_type,
+                   car_type,
+                   max_acc,
+                   pos_res,
+                   vel_res,
+                   s_start,
+                   s_goal,
+                   off_grid,
+                   ΔT,
+                   p_birth,
+                   a_noise,
+                   pos_obs_noise,
+                   vel_obs_noise,
+                   collision_cost,
+                   action_cost,
+                   goal_reward,
+                   γ)
+end
+
+### REWARD MODEL ##################################################################################
+
+function POMDPs.reward(pomdp::OIPOMDP, s::OIState, a::OIAction, sp::OIState)
+    r = 0.
+    if sp.crash
+        r += pomdp.collision_cost
+    end
+    if sp.ego.posF.s >= pomdp.s_goal 
+        r += pomdp.goal_reward
+    elseif a.acc > 0.
+        r += pomdp.action_cost
+    else
+        r += pomdp.action_cost
+    end
+    return r
+end
+
+# other method for SARSOP
+function POMDPs.reward(pomdp::OIPOMDP, s::OIState, a::OIAction)
+    return reward(pomdp, s, a, s)
+end
+
+function POMDPs.isterminal(pomdp::OIPOMDP, s::OIState)
+    return s.crash || s.ego.posF.s >= pomdp.s_goal
+end
+
+######## DISTRIBUTION ############################################################################
+
+"""
+Concrete type to represent a distribution over state for the OIcludedCrosswalk POMDP Problem
+"""
+mutable struct OIDistribution
+    p::Vector{Float64}
+    it::Vector{OIState}
+end
+
+OIDistribution() = OIDistribution(Float64[], OIState[])
+
+POMDPs.iterator(d::OIDistribution) = d.it
+
+# transition and observation pdf
+function POMDPs.pdf(d::OIDistribution, s::OIState)
+    for (i, sp) in enumerate(d.it)
+        if sp==s
+            return d.p[i]
+        end
+    end
+    return 0.
+end
+
+function POMDPs.rand(rng::AbstractRNG, d::OIDistribution)
+    ns = sample(d.it, Weights(d.p)) # sample a neighbor state according to the distribution c
+    return ns
+end
+
+"""
+    most_likely_state(d::OIDistribution)
+returns the most likely state given distribution d
+"""
+function most_likely_state(d::OIDistribution)
+    val, ind = findmax(d.p)
+    return d.it[ind]
+end
+
+### HELPERS
+
+function POMDPs.discount(pomdp::OIPOMDP)
+    return pomdp.γ
+end
+
+function state_to_scene(pomdp::OIPOMDP, s::OIState)
+    scene = Scene()
+    ego = Vehicle(s.ego, pomdp.ego_type, 1)
+    car = Vehicle(s.car, pomdp.car_type, 2)
+    push!(scene, ego)
+    push!(scene, car)
+    return scene
+end
---
+++ single_intersection/spaces.jl
@@ -0,0 +1,264 @@
+#### STATE SPACE ############################
+
+const LANE_ID_LIST = ["right_from_left", "straight_from_right", "left_from_right", "straight_from_left"]
+
+function POMDPs.states(pomdp::OIPOMDP)
+    env = pomdp.env
+    V = linspace(0, pomdp.env.params.speed_limit, Int(floor(pomdp.env.params.speed_limit/pomdp.vel_res)) + 1)
+    S_ego = linspace(pomdp.s_start, pomdp.s_goal, Int(floor((pomdp.s_goal - pomdp.s_start)/pomdp.pos_res)) + 1)
+    state_space = Vector{OIState}()
+    for lane_id in LANE_ID_LIST
+        s_end = get_end(env.lane_map[lane_id])
+        S = linspace(0, s_end, Int(floor(s_end/pomdp.pos_res)) + 1)
+        for s_ego in S_ego
+            for v_ego in V
+                for s in S
+                    for v in V
+                        ego = ego_state(pomdp, s_ego, v_ego)
+                        car = car_state(pomdp, lane_id, s, v)
+                        push!(state_space, OIState(is_crash(pomdp, ego, car), ego, car))
+                    end
+                end
+            end
+        end
+    end
+    for s_ego in S_ego
+        for v_ego in V
+            ego = ego_state(pomdp, s_ego, v_ego)
+            car = get_off_the_grid(pomdp)
+            push!(state_space, OIState(false, ego, car))
+        end
+    end
+    return state_space
+end
+
+function POMDPs.n_states(pomdp)
+    N = 0
+    n_vel = Int(floor(pomdp.env.params.speed_limit/pomdp.vel_res)) + 1
+    n_s_ego = Int(floor((pomdp.s_goal - pomdp.s_start)/pomdp.pos_res)) + 1
+    for lane_id in LANE_ID_LIST
+        s_end = get_end(pomdp.env.lane_map[lane_id])
+        N += (Int(floor(s_end/pomdp.pos_res)) + 1)*n_vel*n_vel*n_s_ego
+    end
+    # add the off the grid state
+    N += n_vel*n_s_ego
+    return N
+end
+
+function POMDPs.state_index(pomdp::OIPOMDP, s::OIState)
+    size_V = Int(floor(pomdp.env.params.speed_limit/pomdp.vel_res)) + 1
+    size_s_ego = Int(floor((pomdp.s_goal - pomdp.s_start)/pomdp.pos_res)) + 1
+
+    s_ego = s.ego.posF.s
+    v_ego = s.ego.v
+    car_lane_tag = s.car.posF.roadind.tag
+    @assert car_lane_tag.segment < 5 #5-6 are reserved for the ego car
+    s_end = get_end(pomdp.env.roadway[car_lane_tag])
+    size_s_car = Int(floor(s_end/pomdp.pos_res)) + 1
+    s_car = s.car.posF.s
+    v_car = s.car.v
+
+    s_ego_ind = Int(ceil((s_ego - pomdp.s_start)/pomdp.pos_res)) + 1
+    v_ego_ind = Int(ceil(v_ego/pomdp.vel_res)) + 1
+    s_car_ind = Int(ceil(s_car/pomdp.pos_res)) + 1
+
+
+    if !off_the_grid(pomdp, s.car)
+        v_car_ind = Int(ceil(v_car/pomdp.vel_res)) + 1
+        ind = sub2ind((size_V, size_s_car, size_V, size_s_ego), v_car_ind, s_car_ind, v_ego_ind, s_ego_ind)
+        for i=1:car_lane_tag.segment-1
+            lane_tag = pomdp.env.lane_map[LANE_ID_LIST[i]].tag
+            s_end = get_end(pomdp.env.roadway[lane_tag])
+            size_s = Int(floor(s_end/pomdp.pos_res)) + 1
+            ind += size_s*size_V*size_s_ego*size_V
+        end
+    else
+        ind = sub2ind((size_V, size_s_ego), v_ego_ind, s_ego_ind)
+        for i=1:length(LANE_ID_LIST)
+            lane_tag = pomdp.env.lane_map[LANE_ID_LIST[i]].tag
+            s_end = get_end(pomdp.env.roadway[lane_tag])
+            size_s = Int(floor(s_end/pomdp.pos_res)) + 1
+            ind += size_s*size_V*size_s_ego*size_V
+        end
+    end
+    return ind
+end
+
+### ACTION SPACE
+POMDPs.actions(pomdp::OIPOMDP) = [OIAction(-4.0),
+                           OIAction(-2.0),
+                           OIAction(0.),
+                           OIAction(1.5),
+                           OIAction(3.0)]
+POMDPs.n_actions(pomdp::OIPOMDP) = 5
+
+function POMDPs.action_index(pomdp::OIPOMDP, a::OIAction)
+    if a.acc == -4.0
+        return 1
+    elseif a.acc == -2
+        return 2
+    elseif a.acc == 0.
+        return 3
+    elseif a.acc == 1.5
+        return 4
+    else
+        return 5
+    end
+end
+
+
+### OBSRVATION SPACE
+
+function POMDPs.observations(pomdp::OIPOMDP)
+    return states(pomdp)
+end
+
+function POMDPs.obs_index(pomdp::OIPOMDP, o::OIObs)
+    return state_index(pomdp, o)
+end
+
+function POMDPs.n_observations(pomdp::OIPOMDP)
+    return n_states(pomdp)
+end
+
+
+#### HELPERS ####
+
+function Base.show(io::IO, s::OIState)
+    print(io, "OIState(", s.ego.posF.s, ", ", s.ego.v, ", ", s.car.posF.roadind.tag, ", ",
+                          s.car.posF.s ,", ", s.car.v, ")")
+end
+
+"""
+    ego_state(pomdp:ICPOMDP, s::Float64, v::Float64)
+Helper that returns the ego car state given its position on the lane and the velocity
+"""
+function ego_state(pomdp::OIPOMDP, s::Float64, v::Float64)
+    ego_lane = pomdp.env.lane_map["ego_left"]
+    posF = Frenet(ego_lane, s)
+    return VehicleState(posF, pomdp.env.roadway, v)
+end
+
+"""
+    car_state(pomdp::OIPOMDP, lane_id::String, s::Float64, v::Float64)
+Helper that returns the state of a car given the lane and the position on the lane
+"""
+function car_state(pomdp::OIPOMDP, lane_id::String, s::Float64, v::Float64)
+    lane = pomdp.env.lane_map[lane_id]
+    posF = Frenet(lane, s)
+    return VehicleState(posF, pomdp.env.roadway, v)
+end
+function car_state(pomdp::OIPOMDP, lane::Lane, s::Float64, v::Float64)
+    posF = Frenet(lane, s)
+    return VehicleState(posF, pomdp.env.roadway, v)
+end
+
+"""
+    is_crash(pomdp::OCPOMDP, ego::VehicleState, car::VehicleState)
+use ADM collision routine to check if a combination of ego car state and car state
+results in a collision
+"""
+function is_crash(pomdp::OIPOMDP, ego::VehicleState, car::VehicleState)
+    return is_colliding(Vehicle(ego, pomdp.ego_type, 0), Vehicle(car, pomdp.car_type, 1))
+end
+
+"""
+    get_off_the_grid(pomdp::OCPOMDP)
+return the off the grid state
+"""
+function get_off_the_grid(pomdp::OIPOMDP)
+    posG = pomdp.off_grid
+    return VehicleState(posG, pomdp.env.roadway, 0.)
+end
+
+"""
+    off_the_grid(pomdp::OCPOMDP, car::VehicleState)
+Check if the current state of the car is in the grid
+"""
+function off_the_grid(pomdp::OIPOMDP, car::VehicleState)
+    return car.posG == pomdp.off_grid
+end
+
+"""
+    get_ego_s(pomdp::OIPOMDP)
+returns all the possible position of the ego car along its lane as a linspace
+"""
+function get_ego_s_grid(pomdp::OIPOMDP, res::Float64 = pomdp.pos_res)
+    return linspace(pomdp.s_start, pomdp.s_goal,
+                    Int(floor((pomdp.s_goal - pomdp.s_start)/pomdp.pos_res)) + 1)
+end
+
+"""
+    get_v_grid(pomdp::OIPOMDP)
+returns all the possible velocities as a linspace
+"""
+function get_v_grid(pomdp::OIPOMDP, res::Float64 = pomdp.vel_res)
+    return linspace(0, pomdp.env.params.speed_limit,
+                    Int(floor(pomdp.env.params.speed_limit/pomdp.vel_res)) + 1)
+end
+
+"""
+    get_lane_s(pomdp::OIPOMDP, lane_id::String)
+returns all the possible position on the corresponding lane
+"""
+function get_lane_s(pomdp::OIPOMDP, lane_id::String, res::Float64 = pomdp.pos_res)
+    s_end = get_end(env.lane_map[lane_id])
+    S = linspace(0, s_end, Int(floor(s_end/res)) + 1)
+    return S
+end
+function get_lane_s(pomdp::OIPOMDP, lane::Lane, res::Float64 = pomdp.pos_res)
+    s_end = get_end(lane)
+    S = linspace(0, s_end, Int(floor(s_end/res)) + 1)
+    return S
+end
+
+function get_end(lane::Lane)
+    s_end = round(lane.curve[end].s)
+    if s_end % 2 == 1
+        s_end -= 1
+    end
+    return s_end
+end
+
+"""
+    car_states(pomdp::OIPOMDP)
+returns a vector of all the states that the other car can occupy
+"""
+function car_states(pomdp::OIPOMDP)
+    env = pomdp.env
+    V = linspace(0, pomdp.env.params.speed_limit, Int(floor(pomdp.env.params.speed_limit/pomdp.vel_res)) + 1)
+    space = Vector{VehicleState}()
+    for lane_id in LANE_ID_LIST
+        s_end = get_end(env.lane_map[lane_id])
+        S = linspace(0, s_end, Int(floor(s_end/pomdp.pos_res)) + 1)
+        for s in S
+            for v in V
+                car = car_state(pomdp, lane_id, s, v)
+                push!(space, car)
+            end
+        end
+    end
+    car = get_off_the_grid(pomdp)
+    push!(space, car)
+    return space
+end
+
+"""
+    n_car_states(pomdp::OIPOMDP)
+returns the number of states a car can occupy
+"""
+function n_car_states(pomdp::OIPOMDP)
+    N = 0
+    n_vel = Int(floor(pomdp.env.params.speed_limit/pomdp.vel_res)) + 1
+    for lane_id in LANE_ID_LIST
+        s_end = get_end(pomdp.env.lane_map[lane_id])
+        N += (Int(floor(s_end/pomdp.pos_res)) + 1)*n_vel
+    end
+    return N+1
+end
+
+function n_ego_states(pomdp::OIPOMDP)
+    n_vel = length(get_v_grid(pomdp))
+    n_s_ego = length(get_ego_s_grid(pomdp))
+    return n_vel*n_s_ego
+end
