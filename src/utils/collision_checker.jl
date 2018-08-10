c64050514efbf67dae7b77f644fed379caf36b9e
MaximeBouton <maxim.bouton@gmail.com>
2018-08-09 17:44:45 -0700

    performance improvements on collision checker

--- src/utils/collision_checker.jl
+++ src/utils/collision_checker.jl
@@ -3,10 +3,12 @@ function collision_checker(veh_a::VehicleState, veh_b::VehicleState, veh_a_def::
     center_a = veh_a.posG
     center_b = veh_b.posG
     # first fast check:
-    Δ² = (veh_a.posG.x - veh_b.posG.x)^2 + (veh_a.posG.y - veh_b.posG.y)^2
-    r_a = sqrt(veh_a_def.length*veh_a_def.length/4 + veh_a_def.width*veh_a_def.width/4)
-    r_b = sqrt(veh_b_def.length*veh_b_def.length/4 + veh_b_def.width*veh_b_def.width/4)
-    if Δ² ≤ r_a*r_a + 2*r_a*r_b + r_b*r_b
+    @fastmath begin
+        Δ = sqrt((veh_a.posG.x - veh_b.posG.x)^2 + (veh_a.posG.y - veh_b.posG.y)^2)
+        r_a = sqrt(veh_a_def.length*veh_a_def.length/4 + veh_a_def.width*veh_a_def.width/4)
+        r_b = sqrt(veh_b_def.length*veh_b_def.length/4 + veh_b_def.width*veh_b_def.width/4)
+    end
+    if Δ ≤ r_a + r_b
         # fast check is true, run parallel axis theorem
         Pa = polygon(center_a, veh_a_def)
         Pb = polygon(center_b, veh_b_def)
@@ -33,24 +35,28 @@ function polygon(x::Float64,y::Float64,theta::Float64,veh_def::VehicleDef)
     l_ = lcar/2
     w_ = wcar/2
 
-    # compute each verteces:
-    P = zeros(4,2)
-    # A, B, C, D = zeros(2), zeros(2), zeros(2), zeros(2)
-    # top left
-    P[1,1] = x + l_*cos(theta) - w_*sin(theta)
-    P[1,2] = y + l_*sin(theta) + w_*cos(theta)
+    # compute each vertex:
+    @fastmath begin 
+        #top left
+        Ax = x + l_*cos(theta) - w_*sin(theta)
+        Ay = y + l_*sin(theta) + w_*cos(theta)
 
-    # top right
-    P[2,1] = x + l_*cos(theta) + w_*sin(theta)
-    P[2,2] = y + l_*sin(theta) - w_*cos(theta)
+        #top right
+        Bx = x + l_*cos(theta) + w_*sin(theta)
+        By = y + l_*sin(theta) - w_*cos(theta)
 
-    # bottom right
-    P[3,1] = x - l_*cos(theta) + w_*sin(theta)
-    P[3,2] = y - l_*sin(theta) - w_*cos(theta)
+        # bottom right
+        Cx = x - l_*cos(theta) + w_*sin(theta)
+        Cy = y - l_*sin(theta) - w_*cos(theta)
 
-    # bottom left
-    P[4,1] = x - l_*cos(theta) - w_*sin(theta)
-    P[4,2] = y - l_*sin(theta) + w_*cos(theta)
+        # bttom left
+        Dx = x - l_*cos(theta) - w_*sin(theta)
+        Dy =  y - l_*sin(theta) + w_*cos(theta)
+    end 
+    P = @SMatrix [Ax Ay; 
+                  Bx By;
+                  Cx Cy;
+                  Dx Dy]
 
     return P
 end
@@ -59,8 +65,8 @@ end
 # polygon a ,
 # polygon described as a nx2 matrix
 
-function overlap(poly_a::Array{Float64,2},
-                poly_b::Array{Float64,2})
+function overlap(poly_a::SMatrix{4, 2, Float64},
+                poly_b::SMatrix{4, 2, Float64})
     """ Check if two convex polygons overlap
     a polygon is a nx2 matrix where n in the number of verteces
         http://gamemath.com/2011/09/detecting-whether-two-convex-polygons-overlap/ """
@@ -76,17 +82,17 @@ function overlap(poly_a::Array{Float64,2},
 
 end
 
-function find_separating_axis(poly_a::Array{Float64,2},
-                              poly_b::Array{Float64,2})
+function find_separating_axis(poly_a::SMatrix{4, 2, Float64},
+                              poly_b::SMatrix{4, 2, Float64})
     """ build a list of candidate separating axes from the edges of a
         /!\ edges needs to be ordered"""
 
     n_a = size(poly_a)[1]
     n_b = size(poly_b)[1]
     axis = zeros(2)
-    previous_vertex = poly_a[end,:]
+    @views previous_vertex = poly_a[end,:]
     for i=1:n_a # loop through all the edges n edges
-        current_vertex = poly_a[i,:]
+        @views current_vertex = poly_a[i,:]
         # get edge vector
         edge = current_vertex - previous_vertex
 
@@ -108,20 +114,20 @@ function find_separating_axis(poly_a::Array{Float64,2},
 
         end
 
-        previous_vertex = poly_a[i,:]
+        @views previous_vertex = poly_a[i,:]
     end
 
     # no separation was found
     return false
 end
 
-function polygon_projection(poly::Array{Float64,2},
+function polygon_projection(poly::SMatrix{4, 2, Float64},
                             axis::Vector{Float64})
         """ return the projection interval for the polygon poly over the axis axis """
 
         n_a = size(poly)[1]
-        d1 = dot(poly[1,:],axis)
-        d2 = dot(poly[2,:],axis)
+        @inbounds @fastmath @views d1 = dot(poly[1,:],axis)
+        @inbounds @fastmath @views d2 = dot(poly[2,:],axis)
         # initialize min and max
         if d1<d2
             out_min = d1
@@ -132,7 +138,7 @@ function polygon_projection(poly::Array{Float64,2},
         end
 
         for i=1:n_a
-            d = dot(poly[i,:],axis)
+            @inbounds @fastmath @views d = dot(poly[i,:],axis)
 
             if d < out_min
                 out_min = d
